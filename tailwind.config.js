34e6b9c65670a78aa468f066e7eaf8fcddfe8fcf
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-05-08 11:23:04 +0900

    feat: design token을 추가한다

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,104 +5,153 @@ module.exports = {
         extend: {
             colors: {
                 'grey-scale': {
-                    'grey-10': '#262626',
-                    'grey-09': '#484848',
-                    'grey-08': '#676767',
-                    'grey-07': '#7b7b7b',
-                    'grey-06': '#a5a5a5',
-                    'grey-05': '#c3c3c3',
-                    'grey-04': '#e5e5e5',
-                    'grey-03': '#f1f1f1',
-                    'grey-02': '#f6f6f6',
-                    'grey-01': '#fbfbfb',
-                    'white': '#ffffff'
+                    10: '#262626',
+                    9: '#484848',
+                    8: '#676767',
+                    7: '#7b7b7b',
+                    6: '#a5a5a5',
+                    5: '#c3c3c3',
+                    4: '#e5e5e5',
+                    3: '#f1f1f1',
+                    2: '#f6f6f6',
+                    1: '#fbfbfb',
+                    white: '#ffffff'
                 },
                 'text': {
-                    'primary': '#262626',
-                    'paragraph01': '#676767',
-                    'paragraph02': '#7b7b7b',
-                    'secondary': '#a5a5a5',
-                    'hint-disabled': '#c3c3c3'
+                    primary: '#262626',
+                    paragraph01: '#676767',
+                    paragraph02: '#7b7b7b',
+                    secondary: '#a5a5a5',
+                    "hint-disabled": '#c3c3c3'
                 },
-                'border': {'border': '#e5e5e5'},
-                'divider': {'divider-01': '#e5e5e5', 'divider-02': '#f6f6f6'},
+                'border': {border: '#e5e5e5'},
+                'divider': {
+                    1: '#e5e5e5',
+                    2: '#f6f6f6'
+                },
                 'red': {
-                    'red-01': '#fef2f2',
-                    'red-02': '#fee2e2',
-                    'red-03': '#fecaca',
-                    'red-04': '#fca5a5',
-                    'red-05': '#f87171',
-                    'red-06': '#ef4444',
-                    'red-07': '#dc2626'
+                    1: '#fef2f2',
+                    2: '#fee2e2',
+                    3: '#fecaca',
+                    4: '#fca5a5',
+                    5: '#f87171',
+                    6: '#ef4444',
+                    7: '#dc2626'
                 },
                 'yellow': {
-                    'yellow-01': '#fefce8',
-                    'yellow-02': '#fef9c3',
-                    'yellow-03': '#fef08a',
-                    'yellow-04': '#fde047',
-                    'yellow-05': '#facc15',
-                    'yellow-06': '#eab308',
-                    'yellow-07': '#ca8a04'
+                    1: '#fefce8',
+                    2: '#fef9c3',
+                    3: '#fef08a',
+                    4: '#fde047',
+                    5: '#facc15',
+                    6: '#eab308',
+                    7: '#ca8a04'
                 },
                 'green': {
-                    'green-01': '#f0fdf4',
-                    'green-02': '#dcfce7',
-                    'green-03': '#bbf7d0',
-                    'green-04': '#86efac',
-                    'green-05': '#4ade80',
-                    'green-06': '#22c55e',
-                    'green-07': '#16a34a'
+                    1: '#f0fdf4',
+                    2: '#dcfce7',
+                    3: '#bbf7d0',
+                    4: '#86efac',
+                    5: '#4ade80',
+                    6: '#22c55e',
+                    7: '#16a34a'
                 },
                 'blue': {
-                    'blue-01': '#eff6ff',
-                    'blue-02': '#dbeafe',
-                    'blue-03': '#bfdbfe',
-                    'blue-04': '#93c5fd',
-                    'blue-05': '#60a5fa',
-                    'blue-06': '#3b82f6',
-                    'blue-07': '#2563eb'
+                    1: '#eff6ff',
+                    2: '#dbeafe',
+                    3: '#bfdbfe',
+                    4: '#93c5fd',
+                    5: '#60a5fa',
+                    6: '#3b82f6',
+                    7: '#2563eb'
                 },
                 'indigo': {
-                    'indigo-01': '#eef2ff',
-                    'indigo-02': '#e0e7ff',
-                    'indigo-03': '#c7d2fe',
-                    'indigo-04': '#a5b4fc',
-                    'indigo-05': '#818cf8',
-                    'indigo-06': '#6366f1',
-                    'indigo-07': '#4f46e5'
+                    1: '#eef2ff',
+                    2: '#e0e7ff',
+                    3: '#c7d2fe',
+                    4: '#a5b4fc',
+                    5: '#818cf8',
+                    6: '#6366f1',
+                    7: '#4f46e5'
                 },
                 'purple': {
-                    'purple-01': '#faf5ff',
-                    'purple-02': '#f3e8ff',
-                    'purple-03': '#e9d5ff',
-                    'purple-04': '#d8b4fe',
-                    'purple-05': '#c084fc',
-                    'purple-06': '#a855f7',
-                    'purple-07': '#9333ea'
+                    1: '#faf5ff',
+                    2: '#f3e8ff',
+                    3: '#e9d5ff',
+                    4: '#d8b4fe',
+                    5: '#c084fc',
+                    6: '#a855f7',
+                    7: '#9333ea'
                 },
                 'pink': {
-                    'pink-01': '#fdf2f8',
-                    'pink-02': '#fce7f3',
-                    'pink-03': '#fbcfe8',
-                    'pink-04': '#f9a8d4',
-                    'pink-05': '#f472b6',
-                    'pink-06': '#ec4899',
-                    'pink-07': '#db2777'
+                    1: '#fdf2f8',
+                    2: '#fce7f3',
+                    3: '#fbcfe8',
+                    4: '#f9a8d4',
+                    5: '#f472b6',
+                    6: '#ec4899',
+                    7: '#db2777'
                 },
                 'positive': {
-                    'positive-01': '#61eeaa',
-                    'positive-02': '#39d979',
-                    'positive-03': '#06c268',
-                    'positive-04': '#05a662'
+                    1: '#61eeaa',
+                    2: '#39d979',
+                    3: '#06c268',
+                    4: '#05a662'
                 },
                 'negative': {
-                    'negative-01': '#ff8c8c',
-                    'negative-02': '#ff5c5c',
-                    'negative-03': '#ff4a4a',
-                    'negative-04': '#ec3939'
+                    1: '#ff8c8c',
+                    2: '#ff5c5c',
+                    3: '#ff4a4a',
+                    4: '#ec3939'
                 }
             },
+            boxShadow: {
+                'shadow-01': '0px 0px 4px 0px rgba(0,0,0,0.1)',
+                'shadow-02': '0px 0px 6px 0px rgba(0,0,0,0.12)',
+                'shadow-03': '0px 0px 8px 0px rgba(0,0,0,0.12)',
+                'shadow-04': '0px 1px 6px 0px rgba(0,0,0,0.14)',
+                'shadow-05': '0px 2px 8px 0px rgba(0,0,0,0.16)'
+            },
+            borderRadius: {
+                none: '0',
+                xs: '0.0625rem',
+                sm: '0.06875000149011612rem',
+                default: '0.125rem',
+                lg: '0.1666666716337204rem',
+                xl: '0.1875rem',
+                '2xl': '0.25rem',
+                '3xl': '0.3125rem',
+                '4xl': '0.375rem',
+                '5xl': '0.5rem',
+                '6xl': '0.625rem',
+                '7xl': '1rem',
+                '8xl': '1.5rem',
+                '9xl': '2.5rem',
+                '10xl': '3.125rem',
+                full: '9999px'
+            },
+            fontFamily: {
+                pretendard: ['Pretendard', 'sans-serif'],
+            },
             fontSize: {
+                h1: ['26px', { lineHeight: '31.2px' }],
+                h2: ['24px', { lineHeight: '28.8px' }],
+                h3: ['20px', { lineHeight: '24px' }],
+                subtitle1: ['18px', { lineHeight: '21.6px' }],
+                subtitle2: ['16px', { lineHeight: '19.2px' }],
+                subtitle3: ['14px', { lineHeight: '16.8px' }],
+                subtitle4: ['12px', { lineHeight: '14.4px' }],
+                body1: ['20px', { lineHeight: '24px' }],
+                body2: ['18px', { lineHeight: '21.6px' }],
+                body3: ['18px', { lineHeight: '28.8px' }],
+                body4: ['16px', { lineHeight: '19.2px' }],
+                body5: ['16px', { lineHeight: '25.6px' }],
+                body6: ['15px', { lineHeight: '18px' }],
+                caption1: ['14px', { lineHeight: '16.8px' }],
+                caption2: ['14px', { lineHeight: '16.8px' }],
+                caption3: ['13px', { lineHeight: '15.6px' }],
+                caption4: ['12px', { lineHeight: '14.4px' }],
+                caption5: ['12px', { lineHeight: '14.4px' }],
                 '3xs': '0.75rem',
                 '2xs': '0.8125rem',
                 'xs': '0.875rem',
@@ -113,32 +162,14 @@ module.exports = {
                 '2xl': '1.5rem',
                 '3xl': '1.625rem'
             },
-            fontFamily: {'pretendard': 'Pretendard'},
-            boxShadow: {
-                'shadow-01': '0px 0px 4px 0px rgba(0,0,0,0.1)',
-                'shadow-02': '0px 0px 6px 0px rgba(0,0,0,0.12)',
-                'shadow-03': '0px 0px 8px 0px rgba(0,0,0,0.12)',
-                'shadow-04': '0px 1px 6px 0px rgba(0,0,0,0.14)',
-                'shadow-05': '0px 2px 8px 0px rgba(0,0,0,0.16)'
+            fontWeight: {
+                bold: 700,
+                semibold: 500,
+                normal: 400,
             },
-            borderRadius: {
-                'none': '0',
-                'xs': '0.0625rem',
-                'sm': '0.06875000149011612rem',
-                'default': '0.125rem',
-                'lg': '0.1666666716337204rem',
-                'xl': '0.1875rem',
-                '2xl': '0.25rem',
-                '3xl': '0.3125rem',
-                '4xl': '0.375rem',
-                '5xl': '0.5rem',
-                '6xl': '0.625rem',
-                '7xl': '1rem',
-                '8xl': '1.5rem',
-                '9xl': '2.5rem',
-                '10xl': '3.125rem',
-                'full': '9999px'
-            }
+            letterSpacing: {
+                tighter: '-0.2px',
+            },
         },
     },
     plugins: [],
