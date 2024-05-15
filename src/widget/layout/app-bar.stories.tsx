85d64d99e99a4a40ca3d7f9d780c21c5a9529b7e
Ji Hyeong Lee <115636461+Jihyeong00@users.noreply.github.com>
2024-05-15 22:15:30 +0900

    feat: Appbaf를 추가합니다.

--- src/widget/layout/app-bar.stories.tsx
+++ src/widget/layout/app-bar.stories.tsx
@@ -7,36 +7,11 @@ const meta = {
   component: AppBar,
   tags: ['autodocs'],
   argTypes: {
-    type: {
-      options: ['default', 'home', 'chat'],
-      control: { type: 'select' },
-      description: '사용되는 페이지의 타입을 보여줍니다.',
-    },
     children: {
-      control: { type: 'text' },
-      type: { name: 'string' },
-      description: '현재 페이지의 이름 혹은 채팅방의 이름을 가리킵니다.',
-    },
-    icon: {
-      type: { name: 'number' },
-      options: [0, 1, 2],
-      control: { type: 'select' },
-      description: '표시할 아이콘의 갯수를 가리킵니다.',
-    },
-    count: {
-      type: { name: 'number' },
-      control: { type: 'number' },
-      description: '채팅방의 수를 나타냅니다.',
-    },
-    line: {
-      type: { name: 'boolean' },
-      control: { type: 'boolean' },
-      description: '하단에 구분선을 표시할지 결정합니다.',
-    },
-    textButton: {
-      control: { type: 'boolean' },
-      table: {
-        disable: true,
+      description: '앱바 아래 분리선을 표시할 지 결정합니다.',
+      control: {
+        type: 'object',
+        disable: false,
       },
     },
   },
--- src/widget/layout/mobile-layout.tsx
+++ src/widget/layout/mobile-layout.tsx
@@ -1,9 +1,15 @@
 import { PropsShouldWithChildren } from '@/share/types'
+import { AppBar } from '@/widget/layout/app-bar'
 
 export function MobileLayout({ children }: PropsShouldWithChildren) {
   return (
     <div className="h-[852px] w-[393px]">
-      <div className="border-grey-scale-grey-05 h-full w-full border-[1px] border-solid">{children}</div>
+      <div className="border-grey-scale-grey-05 h-full w-full border-[1px] border-solid">
+        <AppBar>
+          <AppBar.DefaultContent icon={2} />
+        </AppBar>
+        {children}
+      </div>
     </div>
   )
 }
